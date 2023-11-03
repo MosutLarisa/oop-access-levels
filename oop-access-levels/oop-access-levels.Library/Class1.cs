573f0997eb896d61d8dc489f72a2c85ec025b59b
MosutLarisa <larisa.mosut@emanuel.ro>
2023-11-03 09:44:21 +0200

    oop-access-levels

---
+++ oop-access-levels/oop-access-levels.Library/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace oop_access_levels.Library
+{
+    public class Car
+    {
+        private string manufacturer = "Renault";
+
+        void PrintManufacturer()
+        {
+            //private field accessible from within the class
+            Console.WriteLine(manufacturer);
+        }
+    }
+}
